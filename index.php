f1db3538329b433efc11fcf62491cc1c626a7e95
root <root@localhost.localdomain>
2021-12-19 09:31:51 -0500

    Responsive

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Chat</title>
 	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="AjaxPush.js"></script>
 </head>
